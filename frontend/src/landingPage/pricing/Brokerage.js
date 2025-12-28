50534d1ab23c0f717739be8f653879d25d5d7d5d
Dipen Bhandari <dipenbhandari205@outlook.com>
2025-12-28 11:24:19 -0600

    Structure and desing of pricing page

--- frontend/src/landingPage/pricing/Brokerage.js
+++ frontend/src/landingPage/pricing/Brokerage.js
@@ -1,7 +1,20 @@
 import React from 'react';
 function Brokerage() {
     return (  
-        <h1>Brokerage</h1>
+        <div className='container'>
+            <div className='row p-5 mt-5 text-center border-top'>
+                <div className='col-8'>
+                   <a href="#" style={{textDecoration:"none"}}><h4>Brokerage Calculator</h4></a>
+                
+                </div>
+                <div className='col-4'>
+                    <a href="#" style={{textDecoration:"none"}}><h4>List of Charges</h4></a>
+
+                </div>
+
+            </div>
+
+        </div>
     );
 }
 
--- frontend/src/landingPage/pricing/Hero.js
+++ frontend/src/landingPage/pricing/Hero.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import OpenAccount from '../OpenAccount';
+import './Pricing.css';
 function Hero() {
     return ( 
-        <h1>Hero</h1>
+        <div className="container p-5 mt-5 ">
+            <div className='row text-center'>
+                <h2>Charges</h2>
+                <p className='text-muted listAll' style={{fontSize:"1.2rem"}}>List of all charges and taxes</p>
+            </div>
+
+            <div className='row p-5 mt-5'>
+                <div className='col-md-4 text-center'>
+                    <img src="/images/pricing0.svg" style={{width:"80%"}}></img>
+                    <h3>Free equity delivery</h3>
+                    <p className='text-muted'> All equity delivery investments (NSE, BSE), are absolutely free — ₹ 0 brokerage. </p>
+                </div>
+                <div className='col-md-4 text-center'>
+                    <img src="/images/other-trades.svg" style={{width:"80%"}}></img>
+                    <h3>Intraday and F&O trades</h3>
+                    <p className='text-muted'> Flat ₹ 20 or 0.03% (whichever is lower) per executed order on intraday trades across equity, currency, and commodity trades. Flat ₹20 on all option trades.  </p>
+                </div>
+                <div className='col-md-4'>
+                    <img src="/images/pricing0.svg" style={{width:"80%"}}></img>
+                    <h3 className='text-center'>Free direct MF </h3>
+                    <p className='text-center text-muted'> All direct mutual fund investments are absolutely free — ₹ 0 commissions & DP charges.  </p>
+                </div>
+
+
+            </div>
+
+        
+            
+        
+           
+        </div>
+      
      );
 }
 

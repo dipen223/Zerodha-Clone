9c04e0d4bdcaed849f8124ac5236191bc7384c24
Dipen Bhandari <dipenbhandari205@outlook.com>
2026-01-28 10:32:46 -0600

    setup buywindow

--- dashboard/src/components/BuyDialog.js
+++ dashboard/src/components/BuyDialog.js
@@ -1,37 +1,60 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import Dialog from "@mui/material/Dialog";
 import GeneralContext from "./GeneralContext";
+import axios from 'axios';
 
 import "./BuyDialog.css";
 
 const BuyDialog = ({ uid }) => {
-  const { closeBuyDialog } = useContext(GeneralContext);
+    const [stockQuantity, setStockQuantity] = useState(1);
+    const [stockPrice, setStockPrice] = useState(0.0);
 
-  return (
-    <Dialog open onClose={closeBuyDialog}>
-      <div className="buy-dialog-container">
 
-        <div className="input-fields">
-          <fieldset>
-            <legend>Qty.</legend>
-            <input type="number" name="qty" id="qty" defaultValue={1} />
-          </fieldset>
+    const handleBuyClick = () => {
+        axios.post("http://localhost:8080/newOrder", {
+            name: uid,
+            qty: stockQuantity,
+            price:stockPrice,
+            mode: "BUY",
+        });
+       closeBuyDialog();
+    }
 
-          <fieldset>
-            <legend>Price</legend>
-            <input type="number" name="price" id="price" defaultValue={0} />
-          </fieldset>
-        </div>
+    const { closeBuyDialog } = useContext(GeneralContext);
 
-        <div className="dialog-buttons">
-          <button className="btn btn-blue">Place Order</button>
-          <button className="btn btn-grey" onClick={closeBuyDialog}>
-            Cancel
-          </button>
-        </div>
-      </div>
-    </Dialog>
-  );
+    return (
+        <Dialog open onClose={closeBuyDialog}>
+            <div className="buy-dialog-container">
+
+                <div className="input-fields">
+                    <fieldset>
+                        <legend>Qty.</legend>
+                        <input type="number" name="qty" id="qty" defaultValue={1} onChange={(e) => setStockQuantity(e.target.value)}
+                            value={stockQuantity}
+                        />
+                    </fieldset>
+
+                    <fieldset>
+                        <legend>Price</legend>
+                        <input type="number" name="price" id="price" defaultValue={0}
+                            onChange={(e) => setStockPrice(e.target.value)
+                            }
+
+                            value={stockPrice}
+
+                        />
+                    </fieldset>
+                </div>
+
+                <div className="dialog-buttons">
+                    <button className="btn btn-blue" onClick={handleBuyClick }>Place Order</button>
+                    <button className="btn btn-grey" onClick={closeBuyDialog}>
+                        Cancel
+                    </button>
+                </div>
+            </div>
+        </Dialog>
+    );
 };
 
 export default BuyDialog;
--- dashboard/src/components/GeneralContext.js
+++ dashboard/src/components/GeneralContext.js
@@ -2,6 +2,7 @@ import React,{useState} from 'react';
 
 import BuyDialog from './BuyDialog';
 
+
 const GeneralContext = React.createContext({
     openBuyDialog:(uid) => {},
     closeBuyDialog: () => {},

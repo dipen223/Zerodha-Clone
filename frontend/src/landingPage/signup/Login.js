0f90eedb0308f0d97ccac32cd57c7de67959099c
Dipen Bhandari <dipenbhandari205@outlook.com>
2026-02-01 15:01:30 -0600

    implement a login functionality

--- frontend/src/landingPage/signup/Login.js
+++ frontend/src/landingPage/signup/Login.js
@@ -1,8 +1,91 @@
-import React from 'react'
+import React,{useState} from 'react';
+import {Link,useNavigate} from "react-router-dom";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
 
 const Login = () => {
+    const [inputValue, setInputValue] = useState({
+          email: "",
+          password: "",
+      });
+      const {email,password} = inputValue;
+
+      const handleOnChange =(e) =>{
+        const { name, value } = e.target;
+        setInputValue({
+            ...inputValue,
+            [name]: value,
+        });
+     };
+
+     const handleError = (err) =>
+    toast.error(err, {
+      position: "bottom-left",
+    });
+  const handleSuccess = (msg) =>
+    toast.success(msg, {
+      position: "bottom-left",
+    });
+
+    const handleSubmit = async(e) =>{
+      e.preventDefault();
+      try{
+        const {data} = await axios.post("http://localhost:4000/login",{
+          ...inputValue,
+        },
+        {withCredentials:true}
+      );
+      console.log(data);
+      const {success,message} = data;
+      if(success){
+        handleSuccess(message);
+        setTimeout(()=>{
+          navigate("/");
+        },1000)
+      }else{
+        handleError(message);
+      }
+      }catch(err){
+        console.log(err);
+      }
+      setInputValue({
+        ...inputValue,
+        email:"",
+        password:"",
+      });
+    };
+
   return (
-    <div>Login</div>
+    <div className='container'>
+          <h1 className='text-center mt-5'>Login</h1>
+          <p className='text-center'>Login to start trading your favorite stocks.</p>
+            <div className='col-md-6 mx-auto'>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group mb-3">
+                        <label htmlFor="exampleInputEmail1" className='mb-1'>Email address</label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" name="email" aria-describedby="emailHelp" placeholder="johndoe@gmail.com"
+                            value={email}
+                            onChange={handleOnChange}
+                        />
+                    </div>
+                    <div className="form-group mb-4">
+                        <label htmlFor="exampleInputPassword1" className='mb-1'>Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" name="password"
+                            value={password}
+                            onChange={handleOnChange}
+                        />
+                    </div>
+                    <button type="submit" class="btn btn-primary">Login</button>
+                    <span className='mx-4'>
+                        New here? Signup here  <Link to={"/singup"}> Signup</Link>
+                    </span>
+                </form>
+                <ToastContainer/>
+            </div>
+
+
+
+    </div>
   )
 }
 

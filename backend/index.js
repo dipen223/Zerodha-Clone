0ed025629f45bf83306728e2d135f2ee78241e61
Dipen Bhandari <dipenbhandari205@outlook.com>
2026-02-02 16:15:11 -0600

    add a dashboard origin for cors

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,8 @@ const uri = process.env.MONGO_URL;
 const app = express();
 
 app.use(cors({
-  origin:"http://localhost:3000",
+  origin:["http://localhost:3000",
+  "http://localhost:3001"],
   credentials:true
 }));
 app.use(bodyParser.json());

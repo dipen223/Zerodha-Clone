237c074156255a0b5689aaaef2d954587dd0fecc
Dipen Bhandari <dipenbhandari205@outlook.com>
2026-02-03 11:10:16 -0600

    write a test case for hero component using JEST

---
+++ frontend/src/test/Hero.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {render,screen} from '@testing-library/react';
+import "@testing-library/jest-dom";
+import Hero from '../landingPage/home/Hero';
+
+// Test Suite
+
+describe('Hero Component',()=>{
+    test('renders hero image',() =>{
+        render(<Hero/>);
+        const heroImage = screen.getByAltText('Hero Image');
+        expect(heroImage).toBeInTheDocument();
+        expect(heroImage).toHaveAttribute('src','/images/landing.png');
+    });
+
+
+})
